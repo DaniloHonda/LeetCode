68972afe10c3e0efcf9ca482a9f47352b3972f5c
Danilo Honda <hondadanilo6@gmail.com>
2025-02-16 13:22:00 -0300

    fixes doubly linked and add linked list with reverse

--- linkedlist.py
+++ linkedlist.py
@@ -1,4 +1,3 @@
-
 class Node:
     def __init__(self, value):
         self.value = value
@@ -13,48 +12,45 @@ class LinkedList:
     def add_front(self, value):
         new_node = Node(value)
         if self.head:
-            new_node.next = self.head
             self.head.prev = new_node
+            new_node.next = self.head  # Corrigido: Removido self.
         else:
-            self.tail = new_node
+            self.tail = new_node  # Se a lista estava vazia, tail também aponta para o novo nó.
         self.head = new_node
-        
 
     def add_end(self, value):
         new_node = Node(value)
         if self.tail:
             self.tail.next = new_node
-            new_node.prev = self.tail
+            new_node.prev = self.tail  # Corrigido: Removido self.
         else:
-            self.head = new_node
+            self.head = new_node  # Se a lista estava vazia, head também aponta para o novo nó.
         self.tail = new_node
 
-    def remove_front(self, value):
-        if not self.head:
+    def remove_front(self):
+        if not self.head:  # Lista vazia
             return None
 
         removed = self.head.value
-        
+        self.head = self.head.next  # Avançamos para o próximo nó
+
         if self.head:
-            self.head = self.head.next
+            self.head.prev = None  # Ajustamos o novo head
         else:
-            self.head.prev = None
-        
+            self.tail = None  # Se a lista ficou vazia, tail também deve ser None
+
         return removed
 
-    def remove_end(self, value):
-
-        if not self.tail:
+    def remove_end(self):
+        if not self.tail:  # Lista vazia
             return None
 
         removed = self.tail.value
+        self.tail = self.tail.prev  # Movemos tail para o nó anterior
 
-        if self.tail: # verifica se esta vazia ou com valor valido
-            self.tail = self.tail.prev
+        if self.tail:
+            self.tail.next = None  # Ajustamos o novo tail
         else:
-            self.tail.next = None
-        
+            self.head = None  # Se a lista ficou vazia, head também deve ser None
+
         return removed
-
-
-
